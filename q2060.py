8d28a1a4da9d7acd7072b578e52f6250d68c2f94
danielicapui <37754709+danielicapui@users.noreply.github.com>
2018-05-31 14:07:12 -0300

    2060 - Bino's Challenge

---
+++ q2060.py
@@ -0,0 +1,20 @@
+def multiplo():
+  data = input().split()
+  dois = 0
+  tres = 0
+  quatro = 0
+  cinco = 0
+  for j in map(int, data):  # transforma os valores da lista data em int(inteiros)
+      # condicional para  checar a quantidade  de divisiveis de 2, 3, 4, 5 nessa lista
+      if j % 2 == 0:
+          dois += 1
+      if j % 4 == 0:
+          quatro += 1
+      if j % 3 == 0:
+          tres += 1
+      if j % 5 == 0:
+          cinco += 1
+  print(dois, "Multiplo(s) de 2")
+  print(tres, "Multiplo(s) de 3")
+  print(quatro, "Multiplo(s) de 4")
+  print(cinco, "Multiplo(s) de 5")
